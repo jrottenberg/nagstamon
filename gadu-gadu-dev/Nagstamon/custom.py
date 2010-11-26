36443aae34a0ed1e5761baf92f3573939a1c783f
mrzeszut <mrzeszut@6be4ef30-7c44-4d34-bc15-5202f58876ba>
2010-11-26 23:46:32 +0000

    removed old gadu-gadu dev branch

--- gadu-gadu-dev/Nagstamon/custom.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-""" Module for implementing custom servers,
-columns and other stuff.
-Imported in nagstamonGUI module.
-"""
-
-from nagstamonActions import register_server
-from nagstamonObjects import GenericServer, NagiosHost, NagiosService
